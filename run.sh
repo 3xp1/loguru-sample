ebd9d5570f69cfdee05f17fcf4148e1b554cb2d8
3xp1 <3klittlepigs@gmail.com>
2024-07-23 10:40:37 +0700

    dd

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Проверка на наличие папки venv
 if [ ! -d "venv" ]; then
     echo "Creating virtual environment..."
     python3 -m venv venv
@@ -9,7 +8,6 @@ fi
 echo "Activating virtual environment..."
 source venv/bin/activate
 
-# Проверка на наличие установленного флага в виртуальном окружении
 if [ ! -f "venv/installed" ]; then
     if [ -f "requirements.txt" ]; then
 		echo "Installing wheel for faster installing"
@@ -24,15 +22,8 @@ else
     echo "Dependencies already installed, skipping installation."
 fi
 
-if [ ! -f ".env" ]; then
-	echo "Copying configuration file"
-	cp .env-example .env
-else
-	echo "Skipping .env copying"
-fi
 
 echo "Starting the bot..."
 python3 main.py
 
 echo "done"
-echo "PLEASE EDIT .ENV FILE"
